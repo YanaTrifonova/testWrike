72a08dfab7ba0757b72b14171371d0e96db0ada2
Iana <trifonova.yana.prof@gmail.com>
2019-10-09 21:19:11 +0300

    typo

--- js/buildTree.js
+++ js/buildTree.js
@@ -1,10 +1,15 @@
 async function buildAndPrintTree() {
     const data = await getData();
-    // save our tree as an object of the window to have an ability to use it without binding
     const tree = new Tree(data);
-    document.getElementById("ascending").addEventListener("click", function(){sort(true, tree)});
-    document.getElementById("descending").addEventListener("click", function(){sort(false, tree)});
-    document.getElementById("searchInput").addEventListener("keydown", function(){search(tree)});
+    document.getElementById("ascending").addEventListener("click", function () {
+        sort(true, tree)
+    });
+    document.getElementById("descending").addEventListener("click", function () {
+        sort(false, tree)
+    });
+    document.getElementById("searchInput").addEventListener("keydown", function () {
+        search(tree)
+    });
     printTree(tree);
 }
 
